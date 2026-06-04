1c78b11b21f5eb6ea58ee5088012d2804e2c84ce
karunakar14 <karunakarreddy42435@gmail.com>
2026-06-04 15:35:13 +0530

    initial commit

---
+++ src/main/java/com/telusko/docker/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.telusko.docker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+	@GetMapping("/")
+	public String welcome() {
+		return "welcome to docker";
+	}
+}
